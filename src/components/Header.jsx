a7b6e92696ed4dac965bc253d9793672dd1ce641
mishinvg <mishinvg@gmail.com>
2024-02-28 20:54:49 +0400

    proect28.02
    Занятие 28.02.2024
    Header
    Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <span className="logo">Магазин</span>
+            </div>
+            <div className="presentation"></div>
+        </header>
+    );
+}
