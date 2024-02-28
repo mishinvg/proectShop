cced0ca2aa5780e33612363c2e178cbd5183fdc4
mishinvg <mishinvg@gmail.com>
2024-02-28 21:03:24 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 
-export defaultfunction App() {
+export default function App() {
     return (
       <div className="wrapper">
         <Header />
