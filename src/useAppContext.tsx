75e138533b3e283c42f576bf58b82b96736d2d29
mishinvg <mishinvg@gmail.com>
2024-03-29 20:44:49 +0400

    proect29.03

--- src/useAppContext.tsx
+++ src/useAppContext.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useContext} from "react";
+import axios from "axios";
 
 const AppContext= React.createContext<any>(null);
 
@@ -12,97 +13,24 @@ export const useAppContext = () =>{
 }
 
 const AppProvider:React.FC<{children:React.ReactNode}> = ({children}) =>{
-    const [items, setItems] = useState<any[]>([
-        {
-          id: 1,
-          title: 'ананас',
-          img: 'ananas.jpeg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros.',
-          category: 'фрукты',
-          price: '700'
-        },
-        {
-          id: 2,
-          title: 'клубника',
-          img: 'клубника.jpeg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim. In ut tellus sed sem placerat sollicitudin. Donec quis mollis dolor.',
-          category: 'ягоды',
-          price: '250'
-        },
-        {
-          id: 3,
-          title: 'малина',
-          img: 'малина.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim. In ut tellus sed sem placerat sollicitudin. Donec quis mollis dolor.',
-          category: 'ягоды',
-          price: '500'
-        },
-        {
-          id: 4,
-          title: 'черника',
-          img: 'черника.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque.',
-          category: 'ягоды',
-          price: '800'
-        },
-        {
-          id: 5,
-          title: 'мандарин',
-          img: 'мандарин.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec.',
-          category: 'фрукты',
-          price: '280'
-        },
-        {
-          id: 6,
-          title: 'апельсин',
-          img: 'апельсин.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim.',
-          category: 'фрукты',
-          price: '260'
-        },
-        {
-          id: 7,
-          title: 'груша',
-          img: 'груша.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim. In ut tellus sed sem placerat sollicitudin.',
-          category: 'фрукты',
-          price: '300'
-        },
-        {
-          id: 8,
-          title: 'морковь',
-          img: 'морковь.png',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim.',
-          category: 'овощи',
-          price: '150'
-        },
-        {
-          id: 9,
-          title: 'лук',
-          img: 'лук.jpeg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim. In ut tellus sed sem placerat sollicitudin. Donec quis mollis dolor.',
-          category: 'овощи',
-          price: '70'
-        },
-        {
-          id: 10,
-          title: 'капуста',
-          img: 'капуста.jpg',
-          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat.',
-          category: 'овощи',
-          price: '80'
-        },
-      ]);
-    
+      const [items, setItems] = useState<any[]>([]);
       const [orders,setOrders]=useState<any[]>([]);
       const [currentItems,setCurrentItems]=useState<any[]>([]);
       const [showFullItem, setShowFullItem]=useState<Boolean>(false);
       const [fullItem,setFullItem]=useState<any>({});
     
       useEffect(()=>{
-        setCurrentItems(items);
-      },[items]);
+        axios
+          .get("http://localhost:3001/items")
+          .then((responce)=>{
+            setItems(responce.data);
+            chooseCategory("all");
+            setCurrentItems(responce.data);
+          })
+          .catch((error)=>{
+            console.log("Ошибка при загрузке данных",error);
+          });
+      },[]);
     
       const deleteOrder=(id:number)=>{
         setOrders(orders.filter((el)=>el.id!==id));
